6910fee87c9ff714c6881aca5828d9b630deefaa
HuguesSerrano <hugues.serrano@epitech.eu>
2023-10-05 05:48:34 +0200

    update

---
+++ ex_01/Gecko.php
@@ -0,0 +1,13 @@
+<?php
+
+class Gecko{
+    public function Gecko()
+     {
+            echo "hello !" . PHP_EOL;
+     }
+}
+
+$bleu= new Gecko();
+$bleu-> Gecko();
+$rouge= new Gecko();
+$rouge-> Gecko();
